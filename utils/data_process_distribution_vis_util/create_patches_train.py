80d2f577e18788b2bf4c3093b1f7a3909d4b00c4
dataplus-2020 <69010509+dataplus-2020@users.noreply.github.com>
2020-08-05 18:04:10 -0400

    Update create_patches_train.py

--- utils/data_process_distribution_vis_util/create_patches_train.py
+++ utils/data_process_distribution_vis_util/create_patches_train.py
@@ -1,10 +1,15 @@
 
 import preprocess_wv_for_windturbine as pp
 
+"""
+first, update the get_args() function in pp to desired paths (make sure to change syn_txt_data).
+this script creates folder yolov3_wnd_code/{syn_txt_dir} containing lists of paths to training imgs/lbls.
+copy/paste these lists into training lists in the original syn_txt_dir folder, then update
+syn_0_xview_number in the main .data file to the new number of training examples.
+"""
+
 syn_args = pp.get_args()
-
 # pp.resize_crop_windturbine(syn_args, px_thres=5)
-
 comment = 'synth'
 
 pp.all_to_trn(syn_args, comment, seed=17, pxs='px5_seed17')
