acda07a61218cd504fb9201c6c453fa942b59cd9
dataplus-2020 <69010509+dataplus-2020@users.noreply.github.com>
2020-08-05 18:14:10 -0400

    Update create_patches.py

--- utils/data_process_distribution_vis_util/create_patches.py
+++ utils/data_process_distribution_vis_util/create_patches.py
@@ -1,15 +1,20 @@
 
 import preprocess_wv_for_windturbine as pp
 
+"""
+first, update the get_args() function in pp to desired paths.
+this script splits the data into training and validation sets and creates folder yolov3_wnd_code/{syn_txt_dir} containing
+lists of paths to the specific training and validation imgs/lbls.
+it also creates a .data file containing metadata and paths to these lists.
+"""
+
 syn_args = pp.get_args()
-# pp.resize_crop_windturbine(syn_args)
 
-'''
-pp.split_syn_wnd_trn_val(syn_args, comment='wnd', seed=17, pxs='px5_seed17')
-
-comment = 'bg'
-# create_syn_data(comment, seed=17)
-pp.create_syn_data(syn_args, comment, pxs='px5_seed17')
-'''
-
+# crop images into patches if necessary
 pp.resize_crop_windturbine(syn_args, px_thres=5)
+
+comment = 'wnd'
+
+pp.split_syn_wnd_trn_val(syn_args, seed=17, pxs='px5_seed17')
+
+pp.create_syn_data(syn_args, comment, pxs='px5_seed17')
