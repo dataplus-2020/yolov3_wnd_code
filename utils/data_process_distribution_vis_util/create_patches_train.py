e3728f39204038db18292693f096d17a68227ae7
dataplus-2020 <69010509+dataplus-2020@users.noreply.github.com>
2020-08-05 18:08:01 -0400

    Update create_patches_train.py

--- utils/data_process_distribution_vis_util/create_patches_train.py
+++ utils/data_process_distribution_vis_util/create_patches_train.py
@@ -9,10 +9,12 @@ syn_0_xview_number in the main .data file to the new number of training examples
 """
 
 syn_args = pp.get_args()
+
+# crop images into patches if necessary
 # pp.resize_crop_windturbine(syn_args, px_thres=5)
+
 comment = 'synth'
 
 pp.all_to_trn(syn_args, comment, seed=17, pxs='px5_seed17')
 
-# create_syn_data(comment, seed=17)
 pp.create_syn_data(syn_args, comment, pxs='px5_seed17')
