c93b6c23423f8d51a6d8262b3e945de10d9d6f7b
dataplus-2020 <69010509+dataplus-2020@users.noreply.github.com>
2020-08-05 18:05:18 -0400

    Update create_patches_val.py

--- utils/data_process_distribution_vis_util/create_patches_val.py
+++ utils/data_process_distribution_vis_util/create_patches_val.py
@@ -1,10 +1,14 @@
 
 import preprocess_wv_for_windturbine as pp
 
+"""
+first, update the get_args() function in pp to desired paths (make sure to change syn_txt_data).
+this script creates folder yolov3_wnd_code/{syn_txt_dir} containing lists of paths to validation imgs/lbls.
+copy/paste these lists into val lists in the original syn_txt_dir folder.
+"""
+
 syn_args = pp.get_args()
-
 pp.resize_crop_windturbine(syn_args, px_thres=5)
-
 comment = 'bg'
 
 pp.all_to_val(syn_args, comment, seed=17, pxs='px5_seed17')
